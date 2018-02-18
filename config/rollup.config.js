e1111ac598501de08003c92e889e3a4801480c76
Zheeeng <hi@zheeeng.me>
2018-02-18 23:57:38 +0800

    Add config file

---
+++ config/rollup.config.js
@@ -0,0 +1,32 @@
+import typescript from 'rollup-plugin-typescript2'
+
+const uglify = require('rollup-plugin-uglify')
+const pkg = require('../package.json')
+
+// relative to project root
+const inputFileName = 'src/index.ts'
+const outputFileName = pkg.main
+const compressedOutputFileName = pkg.main.replace(/\.js$/, '.min.js')
+const umdNamespace = 'exportFromJSON'
+const umdTSConfig = 'config/tsconfig.umd.json'
+
+const createUMDTemplate = (isUglify = false) => ({
+  input: inputFileName,
+  output: {
+    file: isUglify ? compressedOutputFileName : outputFileName,
+    format: 'umd',
+    name: umdNamespace,
+  },
+  plugins: [
+    typescript({
+      tsconfig: umdTSConfig,
+    }),
+  ].concat(isUglify ? [uglify()] : []),
+})
+
+const config = [
+  createUMDTemplate(false),
+  createUMDTemplate(true),
+]
+
+export default config

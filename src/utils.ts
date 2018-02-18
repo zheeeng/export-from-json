38f5441f043f4294781913b4e1d0cf2cf7ebcce8
Zheeeng <hi@zheeeng.me>
2018-02-18 23:59:12 +0800

    Add source code

---
+++ src/utils.ts
@@ -0,0 +1,17 @@
+export function assertIsArray (data: any[], message: string) {
+  if (Object.prototype.toString.call(data) !== '[object Array]') {
+    throw new Error(message)
+  }
+}
+
+export function getValues<T> (data: { [key: string]: T }) {
+  return Object.keys(data).map(key => data[key])
+}
+
+export function getKeys<T> (data: { [key: string]: T }) {
+  return Object.keys(data)
+}
+
+export function getEntries<T> (data: { [key: string]: T }) {
+  return Object.keys(data).map(key => [key, data[key]] as [string, T])
+}

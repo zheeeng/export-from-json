8f734ba43e3daa571514ceb1fe81051c7eb26f6b
Zheeeng <hi@zheeeng.me>
2018-02-21 22:57:22 +0800

    Simplify test

--- __tests__/createTableMap.spec.ts
+++ __tests__/createTableMap.spec.ts
@@ -1,30 +1,26 @@
-import { RawData, createTableMap } from '../src/index'
+import { RawData, createTableMap } from '../src/exportFromJSON'
 
-describe('table map is valid', () => {
-  const tableItems: RawData = [{ key: 'hello', value: 42 }, { foo: 'foo' }, { bar: 'bar' }]
-  const tableMap = createTableMap(tableItems)
+describe('createTableMap', () => {
+  it('works as expected', () => {
+    const tableItems: RawData = [
+      { greeting: 'hello' },
+      { foo: 'foo', bar: 'bar' },
+      { a: 100, b: [ 1, 2, 3 ], c: { x: 200, y: 300 }, d: 'D', e: null, f: undefined },
+    ]
+    const tableMap = createTableMap(tableItems)
 
-  test('is plain object', () => {
-    expect(tableMap).toEqual(expect.any(Object))
-  })
+    const expectedTableMap = {
+      greeting: ['hello', '', ''],
+      foo: ['', 'foo', ''],
+      bar: ['', 'bar', ''],
+      a: ['', '', '100'],
+      b: ['', '', JSON.stringify([1, 2, 3])],
+      c: ['', '', JSON.stringify({ x: 200, y: 300 })],
+      d: ['', '', 'D'],
+      e: ['', '', 'null'],
+      f: ['', '', ''],
+    }
 
-  test('the table keys collect all keys of each table item', () => {
-    const expectedTableKeys = tableItems.map(Object.keys).reduce((acc, curr) => acc.concat(curr), [])
-
-    expect(Object.keys(tableMap)).toEqual(expectedTableKeys)
-  })
-
-  test('all values are array type', () => {
-    Object.values(tableMap).forEach(tableMapValue => {
-      expect(tableMapValue).toEqual(expect.any(Array))
-    })
-  })
-
-  test('all values have the same length with the number of table items', () => {
-    const expectedLength = tableItems.length
-
-    Object.values(tableMap).forEach(tableMapValue => {
-      expect(tableMapValue).toHaveLength(expectedLength)
-    })
+    expect(tableMap).toEqual(expectedTableMap)
   })
 })

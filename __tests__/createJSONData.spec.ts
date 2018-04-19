fb8286b1e3b2e5cd85d03c4613a030ec34a04658
Zheeeng <hi@zheeeng.me>
2018-04-19 11:08:32 +0800

    Fix calling signature mismatching

--- __tests__/createJSONData.spec.ts
+++ __tests__/createJSONData.spec.ts
@@ -4,7 +4,7 @@ import { _createJSONData } from '../src/converters'
 describe('_createJSONData', () => {
 
   it('convert plain object as the JSON.stringify does', () => {
-    expect(_createJSONData(testTableItems)).toMatchSnapshot()
+    expect(_createJSONData(testTableItems, null, 0)).toMatchSnapshot()
   })
 
   it('handle toJSON correctly', () => {
@@ -25,7 +25,7 @@ describe('_createJSONData', () => {
 
     const parsedAs2 = { foo: 42, bar: 24 }
 
-    expect(JSON.parse(_createJSONData(toJSON1))).toEqual(parsedAs1)
-    expect(JSON.parse(_createJSONData(toJSON2))).toEqual(parsedAs2)
+    expect(JSON.parse(_createJSONData(toJSON1, null, 0))).toEqual(parsedAs1)
+    expect(JSON.parse(_createJSONData(toJSON2, null, 0))).toEqual(parsedAs2)
   })
 })

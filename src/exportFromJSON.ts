63d5cb69503bb415b28da3d0b38bcbd073b774da
Zheeeng <hi@zheeeng.me>
2018-08-23 11:35:15 +0800

    Move dataURI generating into downler processor

--- src/exportFromJSON.ts
+++ src/exportFromJSON.ts
@@ -3,59 +3,47 @@ import { downloadFile } from './processors'
 import { _createJSONData, createCSVData, createXLSData } from './converters'
 import ExportType from './ExportType'
 
-export interface IOption {
+export interface IOption<R> {
   data: object
   fileName?: string
   exportType?: ExportType
   replacer?: ((key: string, value: any) => any) | Array<number | string> | null,
   space?: string | number
-  processor?: (data: string, fileName: string) => void
+  processor?: (data: string, type: ExportType, fileName: string) => R
 }
 
-function exportFromJSON ({
+function exportFromJSON<R> ({
   data,
   fileName = '',
   exportType = 'json',
   replacer = null,
   space = 4,
-  processor = downloadFile,
-}: IOption) {
+  processor = downloadFile as any,
+}: IOption<R>): R {
   const MESSAGE_IS_ARRAY_FAIL =
-    'Invalid export data. Please provide an array of JSON object'
+    'Invalid export data. Please provide an array of object'
   const MESSAGE_UNKNOWN_EXPORT_TYPE = `Can't export unknown data type ${exportType}.`
 
   const JSONData = _createJSONData(data, replacer, space)
 
   switch (exportType) {
     case 'txt': {
-      const content = JSONData
-      const dataURI = 'data:text/plain;charset=utf-8,' + encodeURIComponent(content)
-      processor(dataURI, normalizeFileName(fileName, 'txt'))
-      break
+      return processor(JSONData, exportType, normalizeFileName(fileName, 'txt'))
     }
     case 'json': {
-      const content = JSONData
-      const dataURI =
-        'data:application/json;charset=utf-8,' + encodeURIComponent(content)
-      processor(dataURI, normalizeFileName(fileName, 'json'))
-      break
+      return processor(JSONData, exportType, normalizeFileName(fileName, 'json'))
     }
     case 'csv': {
       assertIsArray(data, MESSAGE_IS_ARRAY_FAIL)
       const content = createCSVData(data as any[])
-      const dataURI =
-        'data:text/csv;charset=utf-8,\uFEFF' + encodeURIComponent(content)
-      processor(dataURI, normalizeFileName(fileName, 'csv'))
-      break
+
+      return processor(content, exportType, normalizeFileName(fileName, 'csv'))
     }
     case 'xls': {
       assertIsArray(data, MESSAGE_IS_ARRAY_FAIL)
       const content = createXLSData(data as any[])
-      const dataURI =
-        'data:application/vnd.ms-excel;charset=utf-8,' +
-        encodeURIComponent(content)
-      processor(dataURI, normalizeFileName(fileName, 'xls'))
-      break
+
+      return processor(content, exportType, normalizeFileName(fileName, 'xls'))
     }
     default:
       throw new Error(MESSAGE_UNKNOWN_EXPORT_TYPE)
--- src/processors.ts
+++ src/processors.ts
@@ -1,4 +1,28 @@
-export function downloadFile (dataURI: string, fileName: string) {
+import ExportType from './ExportType'
+
+export function generateDataURI (content: string, type: ExportType): string {
+  switch (type) {
+    case 'txt': {
+      return 'data:text/plain;charset=utf-8,' + encodeURIComponent(content)
+    }
+    case 'json': {
+      return 'data:application/json;charset=utf-8,' + encodeURIComponent(content)
+    }
+    case 'csv': {
+      return 'data:text/csv;charset=utf-8,\uFEFF' + encodeURIComponent(content)
+    }
+    case 'xls': {
+      return 'data:application/vnd.ms-excel;charset=utf-8,' + encodeURIComponent(content)
+    }
+    default : {
+      return ''
+    }
+  }
+}
+
+export function downloadFile (content: string, type: ExportType, fileName: string = 'download'): void {
+  const dataURI = generateDataURI(content, type)
+
   const anchor = document.createElement('a')
   anchor.href = dataURI
 
